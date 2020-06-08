eefc9eec6bd76f39261ff7f7d1b534ad1cbd87bd
Michael Waide <mvwaide@gmail.com>
2020-06-08 19:09:47 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
-# code your #position_taken? method here!
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
